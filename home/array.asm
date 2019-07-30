e27ada995c63b68030cf33a3664fb20876b48e4c
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2019-07-30 08:18:26 -0300

    QoL [13]: Faster multiplication (AddNTimes)

--- home/array.asm
+++ home/array.asm
@@ -35,10 +35,20 @@ SkipNames::
 
 AddNTimes::
 ; Add bc * a to hl.
+; Preserves bc
 	and a
 	ret z
+
+	push bc
 .loop
+	rra ; and a from below and above resets carry
+	jr nc, .noadd
 	add hl, bc
-	dec a
+.noadd
+	sla c
+	rl b
+	and a
 	jr nz, .loop
+.done
+	pop bc
 	ret
