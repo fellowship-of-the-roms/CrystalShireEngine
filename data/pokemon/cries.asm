50f939ee200de6809f3940634ebc4b0992ca5928
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2019-08-25 20:07:45 -0300

    Use the 16-bit index to load Pokémon cries, dex entries and footprints

--- data/pokemon/cries.asm
+++ data/pokemon/cries.asm
@@ -258,8 +258,3 @@ PokemonCries::
 	mon_cry CRY_AIPOM,         0,  384 ; HO_OH
 	mon_cry CRY_ENTEI,       330,  273 ; CELEBI
 	assert_table_length NUM_POKEMON
-	mon_cry CRY_NIDORAN_M,     0,    0 ; 252
-	mon_cry CRY_NIDORAN_M,     0,    0 ; 253
-	mon_cry CRY_NIDORAN_M,     0,    0 ; 254
-	mon_cry CRY_NIDORAN_M,     0,    0 ; 255
-	assert_table_length $ff
