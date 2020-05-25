d5c1a8798b47ad640aeed9c4c80a7f37fb48fae3
Rangi <remy.oukaour+rangi42@gmail.com>
2020-05-25 00:18:24 -0400

    Optimize GetSquareRoot (from Prism)

--- engine/math/get_square_root.asm
+++ engine/math/get_square_root.asm
@@ -1,30 +1,19 @@
-DEF NUM_SQUARE_ROOTS EQU 255
-
 GetSquareRoot:
 ; Return the square root of de in b.
-
-; Rather than calculating the result, we take the index of the
-; first value in a table of squares that isn't lower than de.
-
-	ld hl, .Squares
-	ld b, 0
+	ld h, d
+	ld l, e
+	ld de, 1
+	ld b, d
 .loop
-; Make sure we don't go past the end of the table.
 	inc b
 	ld a, b
-	cp NUM_SQUARE_ROOTS
+	inc a
 	ret z
-
-; Iterate over the table until b**2 >= de.
-	ld a, [hli]
-	sub e
-	ld a, [hli]
-	sbc d
-
-	jr c, .loop
+	dec e
+	dec de
+	add hl, de
+	ret nc
+	ld a, h
+	or l
+	jr nz, .loop
 	ret
-
-.Squares:
-for x, 1, NUM_SQUARE_ROOTS + 1
-	dw x**2
-endr
