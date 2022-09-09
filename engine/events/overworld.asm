167c098a31bb2a049b927f4ce1d04ae3674b6280
vulcandth <vulcandth@gmail.com>
2022-09-08 21:26:23 -0500

    Vanilla Bugfix: You can fish on top of NPCs

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -1437,7 +1437,6 @@ FishFunction:
 	dw .FishNoFish
 
 .TryFish:
-; BUG: You can fish on top of NPCs (see docs/bugs_and_glitches.md)
 	ld a, [wPlayerState]
 	cp PLAYER_SURF
 	jr z, .fail
@@ -1446,7 +1445,9 @@ FishFunction:
 	call GetFacingTileCoord
 	call GetTileCollision
 	cp WATER_TILE
-	jr z, .facingwater
+	jr nz, .fail
+	farcall CheckFacingObject
+	jr nc, .facingwater
 .fail
 	ld a, $3
 	ret
