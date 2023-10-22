GlitchR9506/pokecrystal16

Update learnsets, egg moves and TMs/HMs to use 16-bit move IDs
This also creates indirection tables for the egg moves and the learnsets,
splitting the tables into two (as they wouldn't have fit in their current
banks otherwise).

engine/pokemon/evolve.asm
@@ -444,9 +444,15 @@ LearnLevelMoves:
 	ld a, [wCurPartyLevel]
 	cp b
 	call GetNextEvoAttackByte
+	ld e, a
+	call GetNextEvoAttackByte
+	ld d, a
 	jr nz, .find_move
 
 	push hl
+	ld h, d
+	ld l, e
+	call GetMoveIDFromIndex
 	ld d, a
 	ld hl, wPartyMon1Moves
 	ld a, [wCurPartyMon]
@@ -502,6 +508,7 @@ FillMoves:
 	pop de
 .GetMove:
 	inc hl
+	inc hl
 .GetLevel:
 	call GetNextEvoAttackByte
 	and a
@@ -521,7 +528,10 @@ FillMoves:
 	push de
 	ld c, NUM_MOVES
 	ldh a, [hTemp]
-	call GetFarByte
+	push hl
+	call GetFarHalfword
+	call GetMoveIDFromIndex
+	pop hl
 	ld b, a
 .CheckRepeat:
 	ld a, [de]
@@ -562,7 +572,10 @@ FillMoves:
 
 .LearnMove:
 	ldh a, [hTemp]
-	call GetFarByte
+	push hl
+	call GetFarHalfword
+	call GetMoveIDFromIndex
+	pop hl
 	ld b, a
 	ld [de], a
 	ld a, [wEvolutionOldSpecies]
@@ -582,7 +595,7 @@ FillMoves:
 	pop hl
 	ld [hl], a
 	pop hl
-	jr .NextMove
+	jp .NextMove
 
 .done
 	pop bc

home/hm_moves.asm
@@ -10,16 +10,19 @@ IsHM::
 	ret
 
 IsHMMove::
+	call GetMoveIndexFromID
+	ld b, h
+	ld c, l
 	ld hl, .HMMoves
-	ld de, 1
-	jp IsInArray
+	ld de, 2
+	jp IsInHalfwordArray
 
 .HMMoves:
-	db CUT
-	db FLY
-	db SURF
-	db STRENGTH
-	db FLASH
-	db WATERFALL
-	db WHIRLPOOL
-	db -1 ; end
+	dw CUT
+	dw FLY
+	dw SURF
+	dw STRENGTH
+	dw FLASH
+	dw WATERFALL
+	dw WHIRLPOOL
+	dw -1 ; end

fellowship-of-the-roms/CrystalShireEngine

Co-Authored-By: aaaaaa123456789 <aaaaaa123456789@acidch.at>

engine/pokemon/evolve.asm
@@ -440,9 +440,15 @@ LearnLevelMoves:
 	ld a, [wCurPartyLevel]
 	cp b
 	call GetNextEvoAttackByte
+	ld e, a
+	call GetNextEvoAttackByte
+	ld d, a
 	jr nz, .find_move
 
 	push hl
+	ld h, d
+	ld l, e
+	call GetMoveIDFromIndex
 	ld d, a
 	ld hl, wPartyMon1Moves
 	ld a, [wCurPartyMon]
@@ -498,6 +504,7 @@ FillMoves:
 	pop de
 .GetMove:
 	inc hl
+	inc hl
 .GetLevel:
 	call GetNextEvoAttackByte
 	and a
@@ -517,7 +524,10 @@ FillMoves:
 	push de
 	ld c, NUM_MOVES
 	ldh a, [hTemp]
-	call GetFarByte
+	push hl
+	call GetFarWord
+	call GetMoveIDFromIndex
+	pop hl
 	ld b, a
 .CheckRepeat:
 	ld a, [de]
@@ -558,7 +568,10 @@ FillMoves:
 
 .LearnMove:
 	ldh a, [hTemp]
-	call GetFarByte
+	push hl
+	call GetFarWord
+	call GetMoveIDFromIndex
+	pop hl
 	ld b, a
 	ld [de], a
 	ld a, [wEvolutionOldSpecies]
@@ -578,7 +591,7 @@ FillMoves:
 	pop hl
 	ld [hl], a
 	pop hl
-	jr .NextMove
+	jp .NextMove
 
 .done
 	pop bc

home/hm_moves.asm
@@ -10,16 +10,19 @@ IsHM::
 	ret
 
 IsHMMove::
+	call GetMoveIndexFromID
+	ld b, h
+	ld c, l
 	ld hl, .HMMoves
-	ld de, 1
-	jp IsInArray
+	ld de, 2
+	jp IsInWordArray
 
 .HMMoves:
-	db CUT
-	db FLY
-	db SURF
-	db STRENGTH
-	db FLASH
-	db WATERFALL
-	db WHIRLPOOL
-	db -1 ; end
+	dw CUT
+	dw FLY
+	dw SURF
+	dw STRENGTH
+	dw FLASH
+	dw WATERFALL
+	dw WHIRLPOOL
+	dw -1 ; end