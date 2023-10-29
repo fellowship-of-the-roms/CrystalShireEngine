SoupPotato/sourcrystal

Vanilla Bugfix: "Smart" AI does not encourage Solar Beam, Flame Wheel, or Moonlight during Sunny Day

data/battle/ai/sunny_day_moves.asm
@@ -1,13 +1,15 @@
 ; AI_SMART prefers these moves during harsh sunlight.
 
 SunnyDayMoves:
-; BUG: "Smart" AI does not encourage Solar Beam, Flame Wheel, or Moonlight during Sunny Day (see docs/bugs_and_glitches.md)
 	db FIRE_PUNCH
 	db EMBER
 	db FLAMETHROWER
+	db SOLARBEAM
 	db FIRE_SPIN
 	db FIRE_BLAST
+	db FLAME_WHEEL
 	db SACRED_FIRE
 	db MORNING_SUN
 	db SYNTHESIS
+	db MOONLIGHT
 	db -1 ; end

fellowship-of-the-roms/CrystalShireEngine

data/battle/ai/sunny_day_moves.asm
@@ -1,13 +1,15 @@
 ; AI_SMART prefers these moves during harsh sunlight.
 
 SunnyDayMoves:
-; BUG: "Smart" AI does not encourage Solar Beam, Flame Wheel, or Moonlight during Sunny Day (see docs/bugs_and_glitches.md)
 	dw FIRE_PUNCH
 	dw EMBER
 	dw FLAMETHROWER
+	dw SOLARBEAM
 	dw FIRE_SPIN
 	dw FIRE_BLAST
+	dw FLAME_WHEEL
 	dw SACRED_FIRE
 	dw MORNING_SUN
 	dw SYNTHESIS
+	dw MOONLIGHT
 	dw -1 ; end