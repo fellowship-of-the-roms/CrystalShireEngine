Ryuzac/pokecrystal-johto

Link Battle/Trade Rooms Genders

data/trainers/attributes.asm
@@ -404,4 +404,10 @@ TrainerClassAttributes:
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
+; Kris
+	db NO_ITEM, NO_ITEM ; items
+	db 25 ; base reward
+	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
+	dw CONTEXT_USE | SWITCH_SOMETIMES
+
 	assert_table_length NUM_TRAINER_CLASSES

data/trainers/party_pointers.asm
@@ -70,4 +70,5 @@ TrainerGroups:
 	dw OfficerGroup
 	dw GruntFGroup
 	dw MysticalmanGroup
+	dw KrisGroup
 	assert_table_length NUM_TRAINER_CLASSES

data/trainers/pic_pointers.asm
@@ -70,4 +70,5 @@ TrainerPicPointers::
 	dba_pic OfficerPic
 	dba_pic GruntfPic
 	dba_pic MysticalmanPic
+	dba_pic KrisPic
 	assert_table_length NUM_TRAINER_CLASSES

fellowship-of-the-roms/CrystalShireEngine

data/trainers/attributes.asm
@@ -404,4 +404,10 @@ TrainerClassAttributes:
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
+; Kris
+	dw NO_ITEM, NO_ITEM ; items
+	db 25 ; base reward
+	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
+	dw CONTEXT_USE | SWITCH_SOMETIMES
+
 	assert_table_length NUM_TRAINER_CLASSES

data/trainers/party_pointers.asm
@@ -70,4 +70,5 @@ TrainerGroups:
 	dba OfficerGroup
 	dba GruntFGroup
 	dba MysticalmanGroup
+	dba KrisGroup
 	assert_table_length NUM_TRAINER_CLASSES

data/trainers/pic_pointers.asm
@@ -70,4 +70,5 @@ TrainerPicPointers::
 	dba OfficerPic
 	dba GruntfPic
 	dba MysticalmanPic
+	dba KrisPic
 	assert_table_length NUM_TRAINER_CLASSES