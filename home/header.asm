SoupPotato/sourcrystal

Switch to PolishedCrystal's Farcall
Co-authored-by: Rangi <35663410+Rangi42@users.noreply.github.com>
Co-Authored-By: Fredrik Ljungdahl <fredde1994@gmail.com>

home/header.asm
@@ -6,7 +6,7 @@ SECTION "rst0", ROM0[$0000]
 
 SECTION "rst8", ROM0[$0008]
 FarCall::
-	jp FarCall_hl
+	jp RstFarCall
 
 SwapHLDE::
 	push de
@@ -22,8 +22,6 @@ Bankswitch::
 	ret
 
 SECTION "rst18", ROM0[$0018]
-BetterFarCall::
-	jp RstBetterFarCall
 
 SECTION "rst20", ROM0[$0020]
 	rst $38

fellowship-of-the-roms/CrystalShireEngine

home/header.asm
@@ -6,7 +6,7 @@ SECTION "rst0", ROM0[$0000]
 
 SECTION "rst8", ROM0[$0008]
 FarCall::
-	jmp FarCall_hl
+	jmp RstFarCall
 
 SwapHLDE::
 	push de
@@ -22,8 +22,6 @@ Bankswitch::
 	ret
 
 SECTION "rst18", ROM0[$0018]
-BetterFarCall::
-	jmp RstBetterFarCall
 
 SECTION "rst20", ROM0[$0020]
 	rst $38