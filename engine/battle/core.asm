SoupPotato/sourcrystal

Vanilla Bugfix: PRZ and BRN stat reductions don't apply to switched Pokémon
This does not affect link battles or Battle Tower battles because those jump from LoadEnemyMon to InitEnemyMon, which already calls ApplyStatusEffectOnEnemyStats.

engine/battle/core.asm
@@ -6434,9 +6434,7 @@ LoadEnemyMon:
 	ld de, wEnemyStats
 	ld bc, NUM_EXP_STATS * 2
 	call CopyBytes
-
-; BUG: PRZ and BRN stat reductions don't apply to switched Pokémon (see docs/bugs_and_glitches.md)
-	ret
+	jp ApplyStatusEffectOnEnemyStats
 
 CheckSleepingTreeMon:
 ; Return carry if species is in the list

fellowship-of-the-roms/CrystalShireEngine

engine/battle/core.asm
@@ -6514,9 +6514,7 @@ LoadEnemyMon:
 	ld de, wEnemyStats
 	ld bc, NUM_EXP_STATS * 2
 	call CopyBytes
-
-; BUG: PRZ and BRN stat reductions don't apply to switched Pokémon (see docs/bugs_and_glitches.md)
-	ret
+	jmp ApplyStatusEffectOnEnemyStats
 
 CheckSleepingTreeMon:
 ; Return carry if species is in the list