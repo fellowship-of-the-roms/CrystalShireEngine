ee18d7bf8eeb5978c586a95587231e56117a021c
vulcandth <vulcandth@gmail.com>
2023-12-28 12:33:27 -0600

    Optimize update_item_descriptions.asm

--- engine/items/update_item_description.asm
+++ engine/items/update_item_description.asm
@@ -14,21 +14,14 @@ UpdateItemBallDescription:
 	ld a, [wMenuSelection]
 	ld h, HIGH(FIRST_BALL_ITEM)
 	ld l, a
-	call GetItemIDFromIndex
-	ld [wCurSpecies], a
-	hlcoord 0, 12
-	lb bc, 4, SCREEN_WIDTH - 2
-	call Textbox
-	ld a, [wMenuSelection]
-	cp -1
-	ret z
-	decoord 1, 14
-	farjp PrintItemDescription
+	jr UpdateDescription16bit
 
 UpdateKeyItemDescription:
 	ld a, [wMenuSelection]
 	ld h, HIGH(FIRST_KEY_ITEM)
 	ld l, a
+; fallthrough
+UpdateDescription16bit:
 	call GetItemIDFromIndex
 	ld [wCurSpecies], a
 	hlcoord 0, 12
