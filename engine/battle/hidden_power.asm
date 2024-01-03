6af81c99295e4dad5fc4a7f74fa52a907ca4f92b
vulcandth <vulcandth@gmail.com>
2024-01-02 22:38:46 -0600

    Update hidden power to use IVs

--- engine/battle/hidden_power.asm
+++ engine/battle/hidden_power.asm
@@ -1,81 +1,109 @@
 HiddenPowerDamage:
-; Override Hidden Power's type and power based on the user's DVs.
-; TODO: Fix hidden power for IVs
+; Override Hidden Power's type and power based on the user's IVs.
 	ld hl, wBattleMonIVs
 	ldh a, [hBattleTurn]
 	and a
-	jr z, .got_dvs
+	jr z, .got_ivs
 	ld hl, wEnemyMonIVs
-.got_dvs
+.got_ivs
 
 ; Power:
-
-; Take the top bit from each stat
-
-	; Attack
-	ld a, [hl]
-	swap a
-	and %1000
-
-	; Defense
+	call GetHPIV
+	and 2
+	rra
 	ld b, a
-	ld a, [hli]
-	and %1000
-	srl a
+	call GetAttackIV
+	and 2
 	or b
-
-	; Speed
 	ld b, a
-	ld a, [hl]
-	swap a
-	and %1000
-	srl a
-	srl a
+	call GetDefenseIV
+	and 2
+	add a
 	or b
-
-	; Special
 	ld b, a
-	ld a, [hl]
-	and %1000
-	rrca
-	rrca
-	rrca
-	and %00011111
-	or b
-
-; Multiply by 5
-	ld b, a
+	call GetSpeedIV
+	and 2
 	add a
 	add a
-	add b
-
-; Add Special & 3
+	or b
 	ld b, a
-	ld a, [hld]
-	and %0011
-	add b
-
-; Divide by 2 and add 30 + 1
-	srl a
+	call GetSpecialAttackIV
+	and 2
+	swap a
+	rra
+	or b
+	ld b, a
+	call GetSpecialDefenseIV
+	and 2
+	swap a
+	or b
+	ld [hMultiplicand + 2], a
+	xor a
+	ld [hMultiplicand + 1], a
+	ld [hMultiplicand + 0], a
+	ld a, 40
+	ld [hMultiplier], a
+	call Multiply
+	ld a, [hProduct + 2]
+	ld [hDividend + 0], a
+	ld a, [hProduct + 3]
+	ld [hDividend + 1], a
+	ld a, 63
+	ldh [hDivisor], a
+	ld b, 2
+	call Divide
+	ld a, [hQuotient + 3]
 	add 30
-	inc a
-
 	ld d, a
 
-; Type:
-
-	; Def & 3
-	ld a, [hl]
-	and %0011
+; Type
+	call GetHPIV
+	and 1
 	ld b, a
-
-	; + (Atk & 3) << 2
-	ld a, [hl]
-	and %0011 << 4
-	swap a
+	call GetAttackIV
+	and 1
+	add a
+	or b
+	ld b, a
+	call GetDefenseIV
+	and 1
 	add a
 	add a
 	or b
+	ld b, a
+	call GetSpeedIV
+	and 1
+	add a
+	add a
+	add a
+	or b
+	ld b, a
+	call GetSpecialAttackIV
+	and 1
+	swap a
+	or b
+	ld b, a
+	call GetSpecialDefenseIV
+	and 1
+	swap a
+	rla
+	or b
+	ld [hMultiplicand + 2], a
+	xor a
+	ld [hMultiplicand + 1], a
+	ld [hMultiplicand + 0], a
+	ld a, 15
+	ld [hMultiplier], a
+	call Multiply
+	ld a, [hProduct + 2]
+	ld [hDividend + 0], a
+	ld a, [hProduct + 3]
+	ld [hDividend + 1], a
+	ld a, 63
+	ldh [hDivisor], a
+	ld b, 2
+	call Divide
+	ld a, [hQuotient + 3]
 
 ; Skip Normal
 	inc a
