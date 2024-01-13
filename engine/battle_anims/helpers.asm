de3f76825c9f1ee561f73e903f98e9b125957b65
8bitZeta <devin94@protonmail.com>
2024-01-13 01:38:48 -0600

    Store Frameset Data & GetBattleAnimFrame Together

--- engine/battle_anims/helpers.asm
+++ engine/battle_anims/helpers.asm
@@ -10,93 +10,15 @@ ReinitBattleAnimFrameset:
 	ld [hl], -1
 	ret
 
-GetBattleAnimFrame:
-.loop
-	ld hl, BATTLEANIMSTRUCT_DURATION
-	add hl, bc
-	ld a, [hl]
-	and a
-	jr z, .next_frame
-	dec [hl]
-	call .GetPointer
-	ld a, [hli]
-	push af
-	jr .okay
-
-.next_frame
-	ld hl, BATTLEANIMSTRUCT_FRAME
-	add hl, bc
-	inc [hl]
-	call .GetPointer
-	ld a, [hli]
-	cp oamrestart_command
-	jr z, .restart
-	cp oamend_command
-	jr z, .repeat_last
-
-	push af
-	ld a, [hl]
-	push hl
-	and ~(Y_FLIP << 1 | X_FLIP << 1)
-	ld hl, BATTLEANIMSTRUCT_DURATION
-	add hl, bc
-	ld [hl], a
-	pop hl
-.okay
-	ld a, [hl]
-	and Y_FLIP << 1 | X_FLIP << 1 ; The << 1 is compensated in the "oamframe" macro
-	srl a
-	ld [wBattleAnimTempFrameOAMFlags], a
-	pop af
-	ret
-
-.repeat_last
-	xor a
-	ld hl, BATTLEANIMSTRUCT_DURATION
-	add hl, bc
-	ld [hl], a
-
-	ld hl, BATTLEANIMSTRUCT_FRAME
-	add hl, bc
-	dec [hl]
-	dec [hl]
-	jr .loop
-
-.restart
-	xor a
-	ld hl, BATTLEANIMSTRUCT_DURATION
-	add hl, bc
-	ld [hl], a
-
-	dec a
-	ld hl, BATTLEANIMSTRUCT_FRAME
-	add hl, bc
-	ld [hl], a
-	jr .loop
-
-.GetPointer:
-	ld hl, BATTLEANIMSTRUCT_FRAMESET_ID
-	add hl, bc
-	ld e, [hl]
-	ld d, 0
-	ld hl, BattleAnimFrameData
-	add hl, de
-	add hl, de
-	ld a, [hli]
-	ld d, [hl]
-	ld e, a
-	ld hl, BATTLEANIMSTRUCT_FRAME
-	add hl, bc
-	ld l, [hl]
-	ld h, 0
-	add hl, hl
-	add hl, de
-	ret
-
 GetBattleAnimOAMPointer:
 	ld l, a
 	ld h, 0
 	ld de, BattleAnimOAMData
+	ld a, [wBattleAnimTempFrameOAMFlags]
+	add a, a
+	jr nc, .ok
+	ld de, BattleAnimOAMData + 4 * NUM_BATTLE_ANIM_OAMSETS
+.ok
 	add hl, hl
 	add hl, hl
 	add hl, de
@@ -177,8 +99,6 @@ endr
 	ret
 
 INCLUDE "data/battle_anims/ball_colors.asm"
-
-INCLUDE "data/battle_anims/framesets.asm"
 
 INCLUDE "data/battle_anims/oam.asm"
 
