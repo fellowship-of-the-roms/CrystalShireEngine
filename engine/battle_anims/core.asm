3fbba754e44090f7233b597ed316564c06758461
8bitZeta <devin94@protonmail.com>
2024-01-13 01:38:48 -0600

    Store Frameset Data & GetBattleAnimFrame Together

--- engine/battle_anims/core.asm
+++ engine/battle_anims/core.asm
@@ -79,7 +79,8 @@ endr
 
 BattleAnimOAMUpdate:
 	call InitBattleAnimBuffer
-	call GetBattleAnimFrame
+	farcall GetBattleAnimFrame
+	ld a, h
 	cp oamwait_command
 	jmp z, .done
 	cp oamdelete_command
--- main.asm
+++ main.asm
@@ -492,6 +492,11 @@ INCLUDE "mobile/mobile_41.asm"
 INCLUDE "engine/gfx/load_overworld_font.asm"
 
 
+SECTION "Move Animation Framesets", ROMX
+
+INCLUDE "engine/battle_anims/framesets.asm"
+
+
 SECTION "Title", ROMX
 
 INCLUDE "engine/movie/title.asm"
